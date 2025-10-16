28b22b26a7b76153d3255c1288caae24c02698c1
Markeith Eley <meleynyc@gmail.com>
2025-10-16 15:54:35 -0400

    Adjusted csv file and added class that will make an object for our csv data

--- src/main/java/com/pluralsight/AccountingLedgerApp.java
+++ src/main/java/com/pluralsight/AccountingLedgerApp.java
@@ -1,12 +1,30 @@
 package com.pluralsight;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class AccountingLedgerApp {
+    //create scanner that can take user input
     public static Scanner scanner = new Scanner(System.in);
 
 
     public static void main(String[] args) {
+
+
+        //intro to user menu
+        //What would you like to do
+        //list options
+        //
+        System.out.println("Welcome to the main menu ");
+        System.out.println("What would you like to do? ");
+        System.out.println("""
+               \tAdd Deposit(A)
+               \tAdd Payment(P)
+               \tView Receipts(V)
+               \tExit menu(E)
+                """);;
+
+
 
 
     }
