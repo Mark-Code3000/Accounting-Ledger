3dccb635af76b2e157b8330a40572c43cb67f199
Markeith Eley <meleynyc@gmail.com>
2025-10-16 19:40:55 -0400

    added functionality making payment

--- src/main/java/com/pluralsight/AccountingLedgerApp.java
+++ src/main/java/com/pluralsight/AccountingLedgerApp.java
@@ -1,6 +1,11 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.SQLOutput;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class AccountingLedgerApp {
@@ -18,7 +23,31 @@ public class AccountingLedgerApp {
 
             switch (option.toUpperCase()) {
                 case "A":
-                    System.out.println("In case A");
+                    System.out.println("What is the vendor?");
+                    String vendor = scanner.nextLine();
+                    System.out.println("What is the item description?");
+                    String description = scanner.nextLine();
+                    System.out.println("How much are you paying?");
+                    double amout = scanner.nextDouble();
+
+                    //the will eat the next like to avoid problems
+                    scanner.nextLine();
+
+                    // creating receipt from user input
+                    Receipts newDeposit = new Receipts(LocalDate.now(),LocalTime.now(),description,vendor,amout);
+
+                    try{
+                        BufferedWriter receiptsCsv = new BufferedWriter(new FileWriter("src/main/resources/receipts.csv",true));
+                        //add new line to csv file
+                        receiptsCsv.newLine();
+                        receiptsCsv.write(newDeposit.getDate()+"|"+newDeposit.getTime()+"|"+newDeposit.getDescription()+"|"+newDeposit.getVendor()+"|"+newDeposit.getAmount());
+                        System.out.println("Thank you for making a deposit");
+                        receiptsCsv.close();
+
+                    }catch (IOException e){
+                        System.out.println("could not complete action"+e);
+                    }
+
                     break;
 
                 case "P":
