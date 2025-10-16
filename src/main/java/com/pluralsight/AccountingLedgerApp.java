b4e7c6667a04ada13d63649911975e23c7c20556
Markeith Eley <meleynyc@gmail.com>
2025-10-16 14:11:52 -0400

    removed unneeded classes

--- src/main/java/com/pluralsight/AccountingLedgerApp.java
+++ src/main/java/com/pluralsight/AccountingLedgerApp.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class AccountingLedgerApp {
+    public static Scanner scanner = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+
+
+    }
 }
